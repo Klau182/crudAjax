e5be099b016e01b02a7376da0e5e84c69088e751
claudia <claudia_figueroai@hotmail.com>
2021-05-26 20:12:33 -0400

    creacion archivo js guardando las variables que obtendran las etiquetas html para darles animacion con js

---
+++ index.js
@@ -0,0 +1,7 @@
+const d = document;
+  $table = document.querySelector("crud-table"),
+  $form = document.querySelector("crud-form"),
+  $title = document.querySelector("crud-title"),
+  $template = document.getElementById("crud-template").content,
+  $fragment = document.createDocumentFragment(),
+  

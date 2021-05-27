b218cd7176a599d8b654b804189924d60fc642ed
claudia <claudia_figueroai@hotmail.com>
2021-05-26 22:16:19 -0400

    se agregan las funciones y metodos de error y success

--- js/index.js
+++ js/index.js
@@ -1,7 +1,36 @@
 const d = document;
-  $table = document.querySelector("crud-table"),
-  $form = document.querySelector("crud-form"),
-  $title = document.querySelector("crud-title"),
-  $template = document.getElementById("crud-template").content,
-  $fragment = document.createDocumentFragment(),
+  $table = d.querySelector("crud-table"),
+  $form = d.querySelector("crud-form"),
+  $title = d.querySelector("crud-title"),
+  $template = d.getElementById("crud-template").content,
+  $fragment = d.createDocumentFragment()
   
+  const ajax = (options) => {
+    let { url, method, success, error, data } = options;
+    const xhr = new XMLHttpRequest();
+
+    xhr.addEventListener("readystatechange",(e) => {
+      if (xhr.readyState !== 4) return;
+      if (xhr.status >= 200 && xhr.status < 300){
+        let json = JSON.parse(xhr.responseText);
+        success(json);
+      }else{
+        let message = xhr.statusText || "Ocurrio un error";
+        error(`Error ${xhr.status}: ${message}`);
+      }
+    });
+    xhr.open(method || "GET", url);
+    xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+    xhr.send(JSON.stringify(data));
+  }
+  const getAll= () => {
+    ajax({
+      method: "GET",
+      url: "http://localhost:3000/santos",
+      success:(res) => { console.log(res) },
+      error:(err) => { console.log(err) },
+      data:null
+    })
+  }
+
+  d.addEventListener("DOMContentLoaded", getAll);
